5e2ca5e71cb7e67f3bcbadedd002c623901bdbd6
rahul-george <rahulgeorge@anoralabs.com>
2025-10-22 19:39:23 +0200

    feat: partial commit for carousal

---
+++ src/carousal.js
@@ -0,0 +1,11 @@
+export class Carousal {
+  constructor(carousal, images, timeout) {
+    this.carousal = carousal;
+    this.images = images;
+    this.timeout = timeout;
+  }
+
+  setup() {
+    // This method generates all the divs and the structure for the carousal
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import { initialize_dropdown } from "./dropdown";
+// import { initialize_dropdown } from "./dropdown";
+// import { Carousal } from "./carousal";
+import "./styles.css";
 
-initialize_dropdown(document);
+// initialize_dropdown(document);
+
+// const carousalElement = document.querySelector(".carousal");
+// const carousal = new Carousal(carousalElement, [], 5);
