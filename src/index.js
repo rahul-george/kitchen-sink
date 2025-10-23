743982139c4f4487c49520e4e3b0146fa9eb133b
rahul-george <rahulgeorge@anoralabs.com>
2025-10-23 15:32:48 +0200

    feat: enabled dropdown widget and also fixed issue where carousal was redered small

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-// import { initialize_dropdown } from "./dropdown";
+import { initialize_dropdown } from "./dropdown";
 import { Carousal } from "./carousal";
 import "./styles.css";
 
-// initialize_dropdown(document);
+initialize_dropdown(document);
 
 const carousal = new Carousal(document, "#carousal_1");
 carousal.setup();
