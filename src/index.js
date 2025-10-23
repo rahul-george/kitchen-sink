59aa42cdbc0a5f3388b89ec9659ff8dc1df44cba
rahul-george <rahulgeorge@anoralabs.com>
2025-10-23 15:52:19 +0200

    feat: deleting stuff from gh-pages

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import { initialize_dropdown } from "./dropdown";
-import { Carousal } from "./carousal";
-import "./styles.css";
-
-initialize_dropdown(document);
-
-const carousal = new Carousal(document, "#carousal_1");
-carousal.setup();
